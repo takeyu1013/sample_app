4ef1f5b8613690f17c5d6210e6681266bb615bf6
takeyu1013 <yuto1013@gmail.com>
2021-12-19 11:13:59 +0900

    ✨ Add static pages

---
+++ src/pages/static_pages/help.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const Help: NextPage = () => {
+  return (
+    <>
+      <h1>Help</h1>
+      <p>
+        To get help on this sample app, see the{" "}
+        <a href="https://github.com/takeyu1013/sample_app">help page</a>.
+      </p>
+    </>
+  );
+};
+
+export default Help;
---
+++ src/pages/static_pages/home.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1>Sample App</h1>
+      <p>This is the home page for the sample application.</p>
+    </>
+  );
+};
+
+export default Home;
