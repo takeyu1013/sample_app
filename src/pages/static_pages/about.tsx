698e4706fd7d142f8ad95768b6009f47c28b616f
takeyu1013 <yuto1013@gmail.com>
2021-12-22 21:53:42 +0900

    ✨ Add an about page

---
+++ src/pages/static_pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is the sample application for the tutorial.</p>
+    </>
+  );
+};
+
+export default About;
