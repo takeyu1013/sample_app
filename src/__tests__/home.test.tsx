dd02e107049acfd830787e6f0190395b0ce06b4c
takeyu1013 <yuto1013@gmail.com>
2021-12-23 23:18:45 +0900

    ✅ Add a test for rendering the title

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -3,12 +3,18 @@
  */
 
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Home from "../pages/static_pages/home";
 
 describe("Home", () => {
   it("should get home", () => {
     render(<Home />);
     expect(screen.getByText("Sample App")).toBeInTheDocument();
+  });
+  it("should render the title", () => {
+    render(<Home />);
+    waitFor(() => {
+      expect(document.title).toEqual("Home");
+    });
   });
 });
