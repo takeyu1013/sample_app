a8c18d0c482d131bc1a1154e95420e82b06df916
takeyu1013 <yuto1013@gmail.com>
2021-12-23 23:16:28 +0900

    🚚 Change the title

--- src/pages/static_pages/home.tsx
+++ src/pages/static_pages/home.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
       <h1>Sample App</h1>
       <p>This is the home page for the sample application.</p>
     </>
