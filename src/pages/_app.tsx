6ecd4c4d9e99cf47d6784955b4234203f0713c04
takeyu1013 <yuto1013@gmail.com>
2022-01-01 18:06:33 +0900

    ✨ Add a header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <div>
+          <Link href="#">
+            <a>Sample App</a>
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link href="#">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a>Help</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a>Log in</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
