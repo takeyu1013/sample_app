da35c7533fdf4dbfa9a6b428e7a733d55ab3411e
takeyu1013 <yuto1013@gmail.com>
2022-01-01 18:07:34 +0900

    ✨ Add a link and logo

--- src/pages/static_pages/home.tsx
+++ src/pages/static_pages/home.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+
 import Head from "../../layouts/Head";
+import logo from "../../../public/nextjs-logotype-light.svg";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head />
-      <h1>Sample App</h1>
-      <p>This is the home page for the sample application.</p>
+      <div>
+        <Head />
+        <h1>Sample App</h1>
+        <h2>This is the home page for the sample application.</h2>
+        <Link href="#">
+          <a>Sign up now!</a>
+        </Link>
+      </div>
+      <Image src={logo} alt="Next.js logo" width={200} />
     </>
   );
 };
