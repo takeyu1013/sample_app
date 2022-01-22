134df75edd5b11a9a40ab5ea572103cdbb0c6510
takeyu1013 <yuto1013@gmail.com>
2022-01-22 19:35:49 +0900

    🔥 Remove an unnecessary div

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -20,25 +20,23 @@ const Signup: NextPage = () => {
     <>
       <Head title="Sign up" />
       <h1>Sign up</h1>
-      <div className="">
-        <form className="mx-auto max-w-xl" onSubmit={handleSubmit(onSubmit)}>
-          <label>Name</label>
-          <input {...register("name")} />
-          <label className="pt-4">Email</label>
-          <input {...register("email")} />
-          <label className="pt-4">Password</label>
-          <input {...register("password")} />
-          <label className="pt-4">Confirmation</label>
-          <input {...register("passwordConfirmation")} />
-          <div className="pt-4">
-            <input
-              className="w-full block mx-auto bg-blue-500 hover:bg-blue-600 rounded text-white px-4 py-2"
-              type="submit"
-              value="Create my accout"
-            />
-          </div>
-        </form>
-      </div>
+      <form className="mx-auto max-w-xl" onSubmit={handleSubmit(onSubmit)}>
+        <label>Name</label>
+        <input {...register("name")} />
+        <label className="pt-4">Email</label>
+        <input {...register("email")} />
+        <label className="pt-4">Password</label>
+        <input {...register("password")} />
+        <label className="pt-4">Confirmation</label>
+        <input {...register("passwordConfirmation")} />
+        <div className="pt-4">
+          <input
+            className="w-full block mx-auto bg-blue-500 hover:bg-blue-600 rounded text-white px-4 py-2"
+            type="submit"
+            value="Create my accout"
+          />
+        </div>
+      </form>
     </>
   );
 };
